a7a417cda0b7b69438313b2785abcb937d1f59ee
Luke Hodkinson <furious.luke@gmail.com>
2020-04-11 19:01:51 +1000

    Swap to reach router

--- src/main/main.js
+++ src/main/main.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
+
 import '../utils/debug'
+
 import {Styletron} from './styletron'
 import {GlobalStyle} from './global-style'
 
 export function Main({theme, children}) {
   return (
     <Styletron theme={theme}>
-      <BrowserRouter>
-        <GlobalStyle>
-          {children}
-        </GlobalStyle>
-      </BrowserRouter>
+      <GlobalStyle>
+        {children}
+      </GlobalStyle>
     </Styletron>
   )
 }
