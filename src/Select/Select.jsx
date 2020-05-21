a9dc2d40364a907edf52b3c82fcd32de26118823
Luke Hodkinson <furious.luke@gmail.com>
2020-05-22 09:19:10 +1000

    Select returns only values

--- src/Select/Select.jsx
+++ src/Select/Select.jsx
@@ -1,11 +1,29 @@
 import * as React from 'react'
 import {Select as BaseSelect} from 'baseui/select'
+import defaultProps from 'baseui/select/default-props'
 
-import {maybe} from 'tidbits/utils/functional'
+import {toArray, isEmpty} from '../utils/primitives'
+import {maybe} from '../utils/functional'
 
-export function Select({onChange, ...props}) {
-  function handleChange({value}) {
-    return maybe(onChange)(value)
+export function Select({
+  onChange,
+  valueKey = defaultProps.valueKey,
+  multi = defaultProps.multi,
+  value,
+  ...props
+}) {
+  function handleChange({value = []}) {
+    let mapped = value.map(v => v[valueKey])
+    if (!multi) {
+      mapped = mapped[0]
+    }
+    return maybe(onChange)(mapped)
   }
-  return <BaseSelect onChange={handleChange} {...props} />
+  function toValue(value) {
+    if (isEmpty(value)) {
+      return value
+    }
+    return toArray(value).map(v => ({[valueKey]: v}))
+  }
+  return <BaseSelect onChange={handleChange} valueKey={valueKey} value={toValue(value)} {...props} />
 }
