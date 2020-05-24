97833a2e5b786dae60735497269a4108808a5961
Luke Hodkinson <furious.luke@gmail.com>
2020-05-25 08:02:56 +1000

    Add util to filter objects

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,3 +1,8 @@
 export function filterKey(object, filter, key = 'label') {
   return filter ? object[key].toLowerCase().indexOf(filter.toLowerCase()) != -1 : true
 }
+
+export function filterObjects(objects = [], objectsToFilter = [], key = 'id') {
+  const filterSet = new Set(objectsToFilter.map(o => o[key]))
+  return objects.filter((a, b) => !filterSet.has(a[key]))
+}
